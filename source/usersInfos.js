8b995ea77d01cf534287a6ccdadd53e217defb9f
Pierre Goemans <p.goemans@hotmail.fr>
2021-09-29 12:24:00 +0200

    add object wildersInfo

---
+++ source/usersInfos.js
@@ -0,0 +1,19 @@
+const wildersInfos ={
+    
+    name: "Mona Lisa",
+    age: "19",
+    birthday: "",
+    pets: false,
+    nameOfPets: "",
+    favoritFood: "",
+    wildSide: "",
+    social: {
+        linkedin: "lien Linkedin",
+        gitHub: "lien gitHub",
+        cvUser: "lien cv",
+    }
+
+
+
+    
+}
