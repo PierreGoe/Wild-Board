3cfcbc7f67f2db71146f7c5a1a301e3ca6ed1799
Guillaume Keerhem <keerhem.guillaume@hotmail.fr>
2021-09-29 12:34:37 +0200

    made the automatic creation of card on js, need to be inplemented in a function for being fully automatic

--- js/index.js
+++ js/index.js
@@ -5,3 +5,19 @@ card.addEventListener("click", flipCard);
 function flipCard() {
     card.classList.toggle("flipCard");
 }
+
+const cardContainer = document.querySelector("card-container");
+
+const cardBody = document.createElement("article");
+cardBody.setAttribute("class", "card");
+cardContainer.appendChild(cardBody);
+
+const cardFront = document.createElement("div");
+cardFront.setAttribute("class", "front");
+// content to add
+cardBody.appendChild(cardFront);
+
+const cardBack = document.createElement("div");
+cardBack.setAttribute("class", "back");
+// content to add
+cardBody.appendChild(cardBack);
