121ecbcc06f32997dc879da1118b3e11cdfc31d4
Pierre Goemans <p.goemans@hotmail.fr>
2021-10-07 11:29:34 +0200

    Add event listner for button moon Walk return to upPage mobile and Desktop

--- js/index.js
+++ js/index.js
@@ -293,3 +293,44 @@ btnFlame.addEventListener('click', function(){
 
 
 
+// fontion pour faire disparaitre le button
+
+
+
+window.addEventListener("scroll", ()=>{
+  console.log(window.scrollY)
+  console.log(window.innerWidth)
+  if ((window.scrollY <= 610 && window.innerWidth <= 400) || window.innerWidth >=400 ) {
+    document.querySelector(".scrollup").style.display = "none"
+  }
+  else{
+    document.querySelector(".scrollup").style.display = "block"
+  }
+})
+
+
+
+
+
+// var scrolled = false;
+
+// window.onscroll = function() {
+//   scrolled = true;
+//   console.log(scrollY)
+//   if (window.scrollY <= 610){
+//     document.querySelector(".scrollup").style.display = "none"
+//   }
+//   else{
+//     document.querySelector(".scrollup").style.display = "block"
+//   }
+
+// }
+
+// setInterval(function(){
+//   if (scrolled) {
+//     scrolled = false;
+//     // scrollFunction()
+//   }
+// }, 400);
+
+
